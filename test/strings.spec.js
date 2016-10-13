f4418a45780988544eeff4434e2d692cdaac9a23
thecodinggame <eric.mignot+2@gmail.com>
2016-10-13 14:13:49 -0400

    about strings

---
+++ test/strings.spec.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+
+describe('string', function() {
+
+    describe('replace', function() {
+
+        it('only replaces first occurence', function() {
+            expect('  ***  '.replace('*', 'A')).to.equal('  A**  ');
+        });
+    });
+});
